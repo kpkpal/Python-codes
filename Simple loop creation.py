9fa77336d6d39f18b294a2db7eb5dfddb7406213
Karthik Palaniswamy <48888134+kpkpal@users.noreply.github.com>
2019-03-24 21:58:57 -0400

    Simple loop creation

---
+++ Simple loop creation.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 24 20:31:17 2019
+
+@author: KPK
+"""
+
+# Loop Creation
+
+while 1:
+    x = int(input("input a number: "))
+    if x < 1:  
+        print ("bad")
+            
+    if x > 10:
+        print("good")
+              
+    if x == 5:
+        print("Correct")
+        break
+    
+
